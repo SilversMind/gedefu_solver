93d003b05f6f75667e572d727cb492149dce7ded
SilversMind <silversmind@gmail.com>
2021-04-20 18:45:49 +0200

    Add combinations module

--- gedefu_solver/src/main.rs
+++ gedefu_solver/src/main.rs
@@ -1,10 +1,16 @@
-use itertools::Combinations;
+mod combinations;
+
 
 fn main() 
 {
 	let plaintext = String::from("LES POTES DE YANNICK SONT ULTRA GENANTS");
 
-	let array = ['A', 'D', 'F', 'G', 'V', 'X'];
-	let iter_array = array.iter();
+	let vec: Vec<char> = vec!['A', 'D', 'F', 'G', 'V', 'X'];
 
+    for comb in combinations::CombinationsWithRepetitions::new(&vec, 2) {
+        for item in &comb {
+            print!("{} ", item)
+        }
+        println!()
+    }
 }
