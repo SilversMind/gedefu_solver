116eb4b7d9e362c3c6b8192d7697bccd45762e65
SilversMind <silversmind@gmail.com>
2021-04-23 17:52:38 +0200

    Reorg

--- gedefu_solver/src/main.rs
+++ gedefu_solver/src/main.rs
@@ -1,9 +1,9 @@
-mod lib;
+mod library;
 
 fn main() {
 	let plaintext = String::from("TON PTIT PULL DE JOLIE");
 	let secret = String::from("HOCUS");
-	let ciphered_text = lib::encrypt_adfgvx(plaintext, secret);
+	let ciphered_text = library::encrypt_adfgvx(plaintext, secret);
 	println!("Ciphered text is: {}", ciphered_text);
 }
 
