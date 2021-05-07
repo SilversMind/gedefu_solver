5ae6062b3538353445248fd2c4c32f0f9b1f7071
SilversMind <silversmind@gmail.com>
2021-05-07 12:09:40 +0200

    Allow to flag GEDEFU

--- gedefu_solver/src/main.rs
+++ gedefu_solver/src/main.rs
@@ -5,28 +5,35 @@ use itertools::Itertools;
 
 
 fn main() {
-
+	let key_lengths: usize = 10;
 	let alphabet = String::from("ABCDEFGHIJ");
-	let key_length: usize = 5;
-	let to_permute = &alphabet[0..key_length];
 	let mut keys: Vec<String> = vec![];
-	for key in to_permute.chars().permutations(key_length).unique(){
-		keys.push(key.iter().collect());
+	for key_length in 1..key_lengths{
+		let to_permute = &alphabet[0..key_length];
+		for key in to_permute.chars().permutations(key_length).unique(){
+			keys.push(key.iter().collect());
+		}
 	}
 
 
 	// println!("{:?}", &perms);
-    let contents = &mut fs::read_to_string("/Users/bouzdi/Hacking/chall_rm/gedefu_solver/gedefu_solver/src/test.txt")
+    let contents = &mut fs::read_to_string("/Users/bouzdi/Hacking/chall_rm/gedefu_solver/gedefu_solver/src/adfgvx.txt")
         .expect("Something went wrong reading the file");
 
 	let secret = String::from("HOCUS");
-	let secret1 = String::from("HOCUS");
 	let ref ciphered_text = library::encrypt_adfgvx(&contents, &secret);
 	let mut frequency: Vec<(char, u32)>;
+	let mut current_key_size: usize = 0;
 	for key in keys {
-		let deciphered_text = library::decrypt_adfgvx(ciphered_text, &key);
+		if current_key_size != key.len(){
+			println!("Testing key of size {}", key.len());
+		}
+		current_key_size = key.len();
+
+
+		let deciphered_text = library::decrypt_adfgvx(&contents, &key);
 		frequency = library::return_frequence(&deciphered_text);
-		if frequency.first().unwrap().1 > 10 {
+		if frequency.first().unwrap().1 > 14 {
 			println!("Values {:?} for key {}", frequency, key);
 		}
 	}
